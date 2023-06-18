3b9af296f2f207e38141c8c5bd180c0bd387c0ae
Swanny <swanny@warp.inc>
2023-06-18 11:22:10 -0400

    css (#4)
    * fixing ci for real
    
    * messing around with styles
    
    * fmt
    
    * fmt and cleanup
    
    * moving to nightly
    
    * adding toolchain file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,39 @@
+use stylist::css;
 use yew::prelude::*;
 
 #[function_component]
 fn App() -> Html {
     html! {
-        <div>
-        <h1>{ "swan.sh" }</h1>
-        <p>{ "Placeholder Site for swan.sh" }</p>
+        <div class={css!(r#"
+            align-items: center;
+            background-color: #282c34;
+            color: #fff;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            position: absolute;
+            width: 300px;
+            height: 200px;
+            z-index: 15;
+            top: 50%;
+            left: 50%;
+            margin: -100px 0 0 -150px; 
+            font-family: 'Roboto Mono', monospace;
+            font-weight: 400;
+            line-height: 1.2;
+
+        "#)}>
+        <h1 class={css!(r#"
+            font-size: 3rem;
+            text-transform: uppercase;
+            margin: 0;
+            padding: 0;
+            "#)}>{ "swan.sh" }</h1>
+        <p class={css!(r#"
+            font-size: 1rem;
+            margin: 0;
+            padding: 0;
+            "#)}>{ "Placeholder Site for swan.sh" }</p>
         </div>
     }
 }

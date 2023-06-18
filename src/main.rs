913526573a511b1e6a352c86fb18d362e76e0786
Swanny <swanny@warp.inc>
2023-06-18 11:07:01 -0400

    messing around with styles

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,41 @@
 use yew::prelude::*;
+use stylist::yew::use_style;
+use stylist::css;
 
 #[function_component]
 fn App() -> Html {
+    let style = use_style!("text-align: center;");
     html! {
-        <div>
-        <h1>{ "swan.sh" }</h1>
-        <p>{ "Placeholder Site for swan.sh" }</p>
+        <div class={css!(r#"
+            align-items: center;
+            background-color: #282c34;
+            color: #fff;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            position: absolute;
+            width: 300px;
+            height: 200px;
+            z-index: 15;
+            top: 50%;
+            left: 50%;
+            margin: -100px 0 0 -150px; 
+            font-family: 'Roboto Mono', monospace;
+            font-weight: 400;
+            line-height: 1.2;
+
+        "#)}>
+        <h1 class={css!(r#"
+            font-size: 3rem;
+            text-transform: uppercase;
+            margin: 0;
+            padding: 0;
+            "#)}>{ "swan.sh" }</h1>
+        <p class={css!(r#"
+            font-size: 1rem;
+            margin: 0;
+            padding: 0;
+            "#)}>{ "Placeholder Site for swan.sh" }</p>
         </div>
     }
 }
